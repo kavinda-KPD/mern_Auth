cfb1727efe632adf3d35be585fdf76a7d4870d51
kavinda-KPD <kavindadissanayake1999@gmail.com>
2023-07-13 15:40:08 +0530

    upto_7- loging & signup token creation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,6 +15,7 @@ const userSchema = new Schema({
   },
 });
 
+//static signup method
 userSchema.statics.signup = async function (email, password) {
   if (!email || !password) {
     throw Error('All fields must be fill');
@@ -38,6 +39,27 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+//static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error('All fields must be fill');
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error('Incorrect Email');
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error('Incorrect Password');
+  }
 
   return user;
 };
